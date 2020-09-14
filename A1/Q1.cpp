e0f3cbe03ab97cacdac219e6f7076240a18708af
haomateach <65306924+haomateach@users.noreply.github.com>
2020-09-13 23:53:28 -0700

    Create Q1.cpp

---
+++ A1/Q1.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ online.
+// Write C++ code in this online editor and run it.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+  
+  return 0;
+}
