124a3c80e3cadde596f2dfc0dd6b7be069138940
haomateach <65306924+haomateach@users.noreply.github.com>
2020-09-13 23:49:13 -0700

    Delete Q1_solution.cpp

--- W3/Q1_solution.cpp
+++
@@ -1,9 +0,0 @@
-// Online C++ compiler to run C++ online.
-// Write C++ code in this online editor and run it.
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!";
-    return 0;
-}
