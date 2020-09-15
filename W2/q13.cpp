7476c5881030e3eb8de5db5ff3a3becc23d8783b
seanmac44 <71198182+seanmac44@users.noreply.github.com>
2020-09-15 14:17:23 -0700

    Create q13.cpp

---
+++ W2/q13.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int speed, time, distance; // three integers
+    speed = 20;
+    time = 10; 
+    distance = speed * time;
+    cout << "The distance is " << distance << endl;
+return 0;
+}
