75813b665fc91b890a97d437acf35dddd4665502
seanmac44 <71198182+seanmac44@users.noreply.github.com>
2020-09-15 14:37:44 -0700

    Create q14.cpp

---
+++ W2/q14.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float force, area, pressure; // three integers
+    force = 172.5;
+    area = 27.5;
+    pressure = area / force;
+    cout << "The pressure is " << pressure << endl;
+return 0;
+}
